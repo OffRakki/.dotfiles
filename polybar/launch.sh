0cea6879d1a84fd3a77d57af8073610da090567a
Fernando Marques <fernandomarques1505@gmail.com>
2021-04-13 01:47:46 -0300

    new file:   alacritty/alacritty.yml 	new file:   bpytop/bpytop.conf 	new file:   bpytop/error.log 	new file:   dmenu-5.0/LICENSE 	new file:   dmenu-5.0/Makefile 	new file:   dmenu-5.0/README 	new file:   dmenu-5.0/arg.h 	new file:   dmenu-5.0/config.def.h 	new file:   dmenu-5.0/config.def.h.orig 	new file:   dmenu-5.0/config.def.h.rej 	new file:   dmenu-5.0/config.h 	new file:   dmenu-5.0/config.mk 	new file:   dmenu-5.0/dmenu 	new file:   dmenu-5.0/dmenu-border-4.9.diff 	new file:   dmenu-5.0/dmenu-center-4.8.diff 	new file:   dmenu-5.0/dmenu-fuzzyhighlight-4.9.diff 	new file:   dmenu-5.0/dmenu-morecolor-20190922-4bf895b.diff 	new file:   dmenu-5.0/dmenu-numbers-4.9.diff 	new file:   dmenu-5.0/dmenu.1 	new file:   dmenu-5.0/dmenu.c 	new file:   dmenu-5.0/dmenu.c.orig 	new file:   dmenu-5.0/dmenu.c.rej 	new file:   dmenu-5.0/dmenu.o 	new file:   dmenu-5.0/dmenu_path 	new file:   dmenu-5.0/dmenu_run 	new file:   dmenu-5.0/drw.c 	new file:   dmenu-5.0/drw.h 	new file:   dmenu-5.0/drw.o 	new file:   dmenu-5.0/stest 	new file:   dmenu-5.0/stest.1 	new file:   dmenu-5.0/stest.c 	new file:   dmenu-5.0/stest.o 	new file:   dmenu-5.0/util.c 	new file:   dmenu-5.0/util.h 	new file:   dmenu-5.0/util.o 	new file:   dunst/dunstrc 	new file:   flavours/base16/sources/templates/list.yaml 	new file:   flavours/config.toml 	new file:   neofetch/config.conf 	new file:   nvim/.netrwhist 	new file:   nvim/general/settings.vim 	new file:   nvim/init.vim 	new file:   nvim/keys/mappings.vim 	new file:   nvim/plugged/auto-pairs 	new file:   nvim/plugged/mru 	new file:   nvim/plugged/nerdtree 	new file:   nvim/plugged/nvim-colorizer.lua 	new file:   nvim/plugged/rainbow 	new file:   nvim/plugged/ranger.vim 	new file:   nvim/plugged/rust.vim 	new file:   nvim/plugged/undotree 	new file:   nvim/plugged/vifm.vim 	new file:   nvim/plugged/vim-airline 	new file:   nvim/plugged/vim-airline-themes 	new file:   nvim/plugged/vim-bufferline 	new file:   nvim/plugged/vim-css-color 	new file:   nvim/plugged/vim-devicons 	new file:   nvim/plugged/vim-fugitive 	new file:   nvim/plugged/vim-nerdtree-syntax-highlight 	new file:   nvim/plugged/vimwiki 	new file:   nvim/themes/airline.vim 	new file:   nvim/vim-plug/plugins.vim 	new file:   polybar/config 	new file:   polybar/launch.sh 	new file:   qutebrowser/autoconfig.yml 	new file:   qutebrowser/bookmarks/urls 	new file:   qutebrowser/config.py 	new file:   qutebrowser/qsettings/QtProject.conf 	new file:   qutebrowser/quickmarks 	new file:   rofi/config.rasi 	new file:   rofi/rofi-menus 	new file:   rofi/scripts/appsmenu.sh 	new file:   rofi/scripts/i3keybindingsmenu.sh 	new file:   rofi/scripts/i3layoutmenu.sh 	new file:   rofi/scripts/mpdmenu.sh 	new file:   rofi/scripts/networkmenu.py 	new file:   rofi/scripts/nmvpnmenu.sh 	new file:   rofi/scripts/powermenu.sh 	new file:   rofi/scripts/promptmenu.sh 	new file:   rofi/scripts/scrotmenu.sh 	new file:   rofi/themes/appsmenu.rasi 	new file:   rofi/themes/flamingo.rasi 	new file:   rofi/themes/i3keybindingsmenu.rasi 	new file:   rofi/themes/i3layoutmenu.rasi 	new file:   rofi/themes/maimmenu.rasi 	new file:   rofi/themes/mpdmenu.rasi 	new file:   rofi/themes/networkmenu.rasi 	new file:   rofi/themes/nmvpnmenu.rasi 	new file:   rofi/themes/onedark.rasi 	new file:   rofi/themes/powermenu.rasi 	new file:   rofi/themes/promptmenu.rasi 	new file:   rofi/themes/scrotmenu.rasi 	new file:   rofi/themes/shared/colorschemes/dark-amber.rasi 	new file:   rofi/themes/shared/colorschemes/dark-steel-blue.rasi 	new file:   rofi/themes/shared/colorschemes/gruvbox.rasi 	new file:   rofi/themes/shared/colorschemes/light-blue.rasi 	new file:   rofi/themes/shared/colorschemes/nord.rasi 	new file:   rofi/themes/shared/option-menu.rasi 	new file:   rofi/themes/shared/resolutions/1366x768.rasi 	new file:   rofi/themes/shared/resolutions/1920x1080.rasi 	new file:   rofi/themes/shared/settings.rasi 	new file:   starship.toml

---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar -r mainbar &
+polybar -r mainbar2 &
+
+echo "Polybar launched..."
