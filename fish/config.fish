cd693620e116e5a4fa49adef4741f19b0c08abfa
Fernando Marques <fernandomarques1505@gmail.com>
2021-08-18 00:49:32 -0300

    modified:   alacritty/alacritty.yml 	modified:   fish/config.fish 	modified:   fish/fish_variables 	new file:   nvim/.nvimchadbkp/init.lua 	new file:   nvim/.nvimchadbkp/lua/chadrc.lua 	new file:   nvim/.nvimchadbkp/lua/highlights.lua 	new file:   nvim/.nvimchadbkp/lua/mappings.lua 	new file:   nvim/.nvimchadbkp/lua/options.lua 	new file:   nvim/.nvimchadbkp/lua/packerInit.lua 	new file:   nvim/.nvimchadbkp/lua/pluginList.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/autopairs.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/autosave.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/bufferline.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/chadsheet.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/compe.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/dashboard.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/gitsigns.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/icons.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/lspconfig.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/luasnip.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/nvimtree.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/others.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/statusline.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/telescope.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/toggleterm.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/treesitter.lua 	new file:   nvim/.nvimchadbkp/lua/plugins/zenmode.lua 	new file:   nvim/.nvimchadbkp/lua/telescope/_extensions/themes.lua 	new file:   nvim/.nvimchadbkp/lua/theme.lua 	new file:   nvim/.nvimchadbkp/lua/themes/chadracula.lua 	new file:   nvim/.nvimchadbkp/lua/themes/everforest.lua 	new file:   nvim/.nvimchadbkp/lua/themes/gruvchad.lua 	new file:   nvim/.nvimchadbkp/lua/themes/javacafe.lua 	new file:   nvim/.nvimchadbkp/lua/themes/mountaineer.lua 	new file:   nvim/.nvimchadbkp/lua/themes/norchad.lua 	new file:   nvim/.nvimchadbkp/lua/themes/one-light.lua 	new file:   nvim/.nvimchadbkp/lua/themes/onedark.lua 	new file:   nvim/.nvimchadbkp/lua/themes/tokyonight.lua 	new file:   nvim/.nvimchadbkp/lua/themes/tomorrow-night.lua 	new file:   nvim/.nvimchadbkp/lua/utils.lua 	new file:   nvim/.nvimchadbkp/plugin/packer_compiled.vim 	modified:   polybar/config

--- fish/config.fish
+++ fish/config.fish
@@ -4,36 +4,36 @@ end
 
 ## VARIABLES
 # Removes greeting message
-set -U fish_greeting
+set -g fish_greeting
 # Vim keys
-set -U fish_vi_key_bindings
+#set -g fish_vi_key_bindings
 
-#set -U fish_color_autosuggestion      brblack
-set -U fish_color_cancel              -r
-set -U fish_color_command             brgreen
-set -U fish_color_comment             brmagenta
-set -U fish_color_cwd                 green
-set -U fish_color_cwd_root            red
-set -U fish_color_end                 brmagenta
-set -U fish_color_error               brred
-set -U fish_color_escape              brcyan
-set -U fish_color_history_current     --bold
-set -U fish_color_host                normal
-set -U fish_color_match               --background=brblue
-set -U fish_color_normal              normal
-set -U fish_color_operator            cyan
-set -U fish_color_param               brblue
-set -U fish_color_quote               yellow
-set -U fish_color_redirection         bryellow
-set -U fish_color_search_match        'bryellow' '--background=brblack'
-set -U fish_color_selection           'white' '--bold' '--background=brblack'
-set -U fish_color_status              red
-set -U fish_color_user                brgreen
-set -U fish_color_valid_path          --underline
-set -U fish_pager_color_completion    normal
-set -U fish_pager_color_description   yellow
-set -U fish_pager_color_prefix        'white' '--bold' '--underline'
-set -U fish_pager_color_progress      'brwhite' '--background=cyan' colors
+#set -g fish_color_autosuggestion      brblack
+set -g fish_color_cancel              -r
+set -g fish_color_command             brgreen
+set -g fish_color_comment             brmagenta
+set -g fish_color_cwd                 green
+set -g fish_color_cwd_root            red
+set -g fish_color_end                 brmagenta
+set -g fish_color_error               brred
+set -g fish_color_escape              brcyan
+set -g fish_color_history_current     --bold
+set -g fish_color_host                normal
+set -g fish_color_match               --background=brblue
+set -g fish_color_normal              normal
+set -g fish_color_operator            cyan
+set -g fish_color_param               brblue
+set -g fish_color_quote               yellow
+set -g fish_color_redirection         bryellow
+set -g fish_color_search_match        'bryellow' '--background=brblack'
+set -g fish_color_selection           'white' '--bold' '--background=brblack'
+set -g fish_color_status              red
+set -g fish_color_user                brgreen
+set -g fish_color_valid_path          --underline
+set -g fish_pager_color_completion    normal
+set -g fish_pager_color_description   yellow
+set -g fish_pager_color_prefix        'white' '--bold' '--underline'
+set -g fish_pager_color_progress      'brwhite' '--background=cyan' colors
 #set -g fish_color_param 616161
 #set -g fish_color_search_match --background=2f134a
 #set -g fish_color_error ff661a
